18faa5cce239c0a0b4d771ddafa4c2102fafcd50
hsuyeung <hsuyeung.com@gmail.com>
2022-04-10 14:41:44 +0800

    Initialize Repository.

---
+++ src/main/java/com/hsuyeung/toolbox/ToolboxApplication.java
@@ -0,0 +1,16 @@
+package com.hsuyeung.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hsuyeung
+ */
+@SpringBootApplication
+public class ToolboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsuyeung/toolbox/ToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsuyeung.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
